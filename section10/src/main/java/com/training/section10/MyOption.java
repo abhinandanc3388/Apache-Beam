8a8630c612ece5b5fa2be23b6d068b5599e03b75
abhinandan <abhinandanc3388@gmail.com>
2022-01-11 14:37:58 +0530

    feature test1

--- section10/src/main/java/com/training/section10/MyOption.java
+++ section10/src/main/java/com/training/section10/MyOption.java
@@ -4,5 +4,10 @@ import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 
 public interface MyOption extends DataflowPipelineOptions {
 
+
+    public void test(){
+        System.out.println("hello");
+    }
+
 }
 
